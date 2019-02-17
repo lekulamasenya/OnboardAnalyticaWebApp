7c66126c1368ea2c2a2fe1fd7efbd7d209b82cf6
Bruce Masenya <bmasenya@iqbusiness.net>
2019-02-18 01:12:54 +0200

    Initial Commit

---
+++ OnboardAnalyticaWebApp.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using OnboardAnalyticaWebApp.API.Models;
+
+namespace OnboardAnalyticaWebApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+        public DbSet<Value> Values {get; set;}
+    }
+}
